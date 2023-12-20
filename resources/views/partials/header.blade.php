4abca538eafaf501539d55d870c53edf9514f606
ericaostini <ericaostini14@gmail.com>
2023-12-20 15:51:19 +0100

    add nav bar and active class in header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,24 @@
-<header>header <i class = "fa-solid fa-home"></i></header>
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <h3 class="mx-3">Shop</h3>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link {{ Route::currentRoutename() == 'home' ? 'active' : '' }}" aria-current="page"
+                            href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Route::currentRoutename() == 'movies.index' ? 'active' : '' }}"
+                            href="{{ route('movies.index') }}">Movies</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+</header>
