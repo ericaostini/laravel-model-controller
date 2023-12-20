07a715b2077a1ed48fa0aab415f71b17ce84b5c9
ericaostini <ericaostini14@gmail.com>
2023-12-20 14:57:37 +0100

    create MovieController and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
