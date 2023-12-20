fc7086d9feb6c4a0107bf57b34eb60a87207e395
ericaostini <ericaostini14@gmail.com>
2023-12-20 15:04:27 +0100

    add show route

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app');
+
+@section('title', 'All movies')
+
+@section('content')
+    @foreach ($movies as $movie)
+        <div>{{ $movie->title }}</div>
+    @endforeach
+@endsection
